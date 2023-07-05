ab8d9d45a9c65e9f9a1b1d0e20356224617a32bd
Serhii <65786724+NorovSergey@users.noreply.github.com>
2023-07-05 13:31:28 +0300

    The task2.py is created and ready for review

---
+++ hw_03/task2.py
@@ -0,0 +1,24 @@
+# Task 2.1
+age = 29
+age_in_month = age * 12
+print(age_in_month)
+
+# Task 2.2
+age_in_years = age_in_month // 12
+print(age_in_years)
+
+# ----------------------------------------------------------------------------------------------------------------------
+# also we can use float() to return a floating-point number from a number or a string representation of a numeric value
+# or int() to return an integer from a given object or converts a number in a given base to a decimal.
+# ----------------------------------------------------------------------------------------------------------------------
+
+# age_in_years = float(age_in_month // 12)
+# print(age_in_years)
+
+# Task 2.3
+my_name = "Serhii"
+# print(my_name)
+
+#Task 2.4
+my_age = 'My name is ' + my_name + ', I am ' + str(age_in_years) + ' years old.'
+print(my_age)
