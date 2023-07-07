5f86d80a7ede638e31433b0fae3b6407f218bfb3
Serhii <65786724+NorovSergey@users.noreply.github.com>
2023-07-07 13:35:33 +0300

    The task1.py is created and ready to review

--- hw_04/task1.py
+++ hw_04/task1.py
@@ -1,8 +1,8 @@
 # Task 1
 value = input('Entered value: ')
 if value.isnumeric():
-    print(f'The enterd value {value} is a number')
+    print(f'The entered value {value} is a number')
 elif value.isalpha():
-    print(f'The enterd value {value} is a text')
+    print(f'The entered value {value} is a text')
 else:
     print(f'You wrote the wrong value {value}')
